b3097a31abcd9c14a20306ca0778b2cc1ec63a51
Gillen Daniel <development@sits.lu>
2024-03-20 14:39:35 +0100

    Fixed AFFv4 library builds on Linux...

--- BuildLibs.sh
+++ BuildLibs.sh
@@ -8,16 +8,31 @@ control_c() {
 
 trap control_c SIGINT
 
-if [[ "$1" != "linux" && "$1" != "osx" ]]; then
-  echo "Usage: $0 <linux | osx> [<jobs>]"
+if [[ "$1" != "debian" && "$1" != "osx" ]]; then
+  echo "Usage: $0 <debian | osx> [<jobs>]"
   exit 1
 fi
 
-PLATFORM="$1"
+OS="$1"
 JOBS=1
 
 if [ ! -z "$2" ]; then
   JOBS=$2
+fi
+
+LINUX_LD_PATH=""
+if [ "$OS" == "debian" ]; then
+  # Need to find system library path
+  ARCH=`uname -m`
+
+  if [ "$ARCH" == "i686" ]; then
+    LINUX_LD_PATH="/lib/i386-linux-gnu"
+  elif [ "$ARCH" == "x86_64" ]; then
+    LINUX_LD_PATH="/lib/x86_64-linux-gnu"
+  else
+    echo "This script only supports i686 and x86_64 architectures!"
+    exit 1
+  fi
 fi
 
 ############################## LibAFF
@@ -57,77 +72,50 @@ fi
 )
 
 # LibRAPTOR2
-if [ "$PLATFORM" == "linux" ]; then
-  (
-    cd libxmount_input/libxmount_input_aff4/
-    echo
-    echo "Extracting libraptor2"
-    rm -rf libraptor2 &>/dev/null
-    tar xfz libraptor2-*.tar.gz
-    echo
-    read -p "Ready to configure LIBRAPTOR2?"
-    cd libraptor2
-    CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure --with-www=none --with-yajl=no --enable-release
-    echo
-    read -p "Ready to compile LIBRAPTOR2?"
-    make -j$JOBS
-    mkdir src/raptor2
-    cp src/raptor2.h src/raptor2/
-  )
-else
-  (
-    cd libxmount_input/libxmount_input_aff4/
-    echo
-    echo "Extracting libraptor2"
-    rm -rf libraptor2 &>/dev/null
-    tar xfz libraptor2-*.tar.gz
-    echo
-    read -p "Ready to configure LIBRAPTOR2?"
-    cd libraptor2
+(
+  cd libxmount_input/libxmount_input_aff4/
+  echo
+  echo "Extracting libraptor2"
+  rm -rf libraptor2 &>/dev/null
+  tar xfz libraptor2-*.tar.gz
+  echo
+  read -p "Ready to configure LIBRAPTOR2?"
+  cd libraptor2
+  #CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure --with-www=none --with-yajl=no --enable-release
+  if [ "$OS" == "debian" ]; then
+    CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure --with-www=none --with-yajl=no --enable-shared=yes --enable-static=yes --enable-release
+  else
     CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure --enable-parsers=turtle --enable-serializers=ntriples --with-www=none --with-yajl=no --enable-release
-    echo
-    read -p "Ready to compile LIBRAPTOR2?"
-    make -j$JOBS
-    mkdir src/raptor2
-    cp src/raptor2.h src/raptor2/
-  )
-fi
+  fi
+  echo
+  read -p "Ready to compile LIBRAPTOR2?"
+  make -j$JOBS
+  mkdir src/raptor2
+  cp src/raptor2.h src/raptor2/
+)
 
 # LibAFF4
-if [ "$PLATFORM" == "linux" ]; then
-  (
-    cd libxmount_input/libxmount_input_aff4/
-    echo
-    echo "Extracting libaff4"
-    rm -rf libaff4 &>/dev/null
-    tar xfz libaff4-*.tar.gz
-    echo
-    read -p "Ready to configure LIBAFF4?"
-    cd libaff4
-    ./autogen.sh
-    CFLAGS="-fPIC -I$(pwd)/../libsnappy -I$(pwd)/../libsnappy/build" CXXFLAGS="-fPIC -std=c++11 -I$(pwd)/../libsnappy -I$(pwd)/../libsnappy/build" LDFLAGS="-L$(pwd)/../libsnappy/build" RAPTOR2_LIBS="-L$(pwd)/../libraptor2/src/.libs -lraptor2" RAPTOR2_CFLAGS="-I$(pwd)/../libraptor2/src" ./configure --disable-shared --enable-static
-    echo
-    read -p "Ready to compile LIBAFF4?"
-    make -j$JOBS
-  )
-else
-  # On OSx, we need some more shenanigans for this to compile...
-  (
-    cd libxmount_input/libxmount_input_aff4/
-    echo
-    echo "Extracting libaff4"
-    rm -rf libaff4 &>/dev/null
-    tar xfz libaff4-*.tar.gz
-    echo
-    read -p "Ready to configure LIBAFF4?"
-    cd libaff4
-    ./autogen.sh
+(
+  cd libxmount_input/libxmount_input_aff4/
+  echo
+  echo "Extracting libaff4"
+  rm -rf libaff4 &>/dev/null
+  tar xfz libaff4-*.tar.gz
+  echo
+  read -p "Ready to configure LIBAFF4?"
+  cd libaff4
+  ./autogen.sh
+  #CFLAGS="-fPIC -I$(pwd)/../libsnappy -I$(pwd)/../libsnappy/build" CXXFLAGS="-fPIC -std=c++11 -I$(pwd)/../libsnappy -I$(pwd)/../libsnappy/build" LDFLAGS="-L$(pwd)/../libsnappy/build" RAPTOR2_LIBS="-L$(pwd)/../libraptor2/src/.libs -lraptor2" RAPTOR2_CFLAGS="-I$(pwd)/../libraptor2/src" ./configure --disable-shared --enable-static
+  if [ "$OS" == "debian" ]; then
+    CFLAGS="-fPIC -I$(pwd)/../libsnappy -I$(pwd)/../libsnappy/build" CXXFLAGS="-fPIC -std=c++11 -I$(pwd)/../libsnappy -I$(pwd)/../libsnappy/build" LDFLAGS="-L$(pwd)/../libsnappy/build" RAPTOR2_LIBS="-L$(pwd)/../libraptor2/src/.libs -lraptor2 -lxml2" RAPTOR2_CFLAGS="-I$(pwd)/../libraptor2/src" ./configure --disable-shared --enable-static
+  else
+    # On OSx, we need even more shenanigans for this to compile...
     CFLAGS="-fPIC -I$(pwd)/../libsnappy -I$(pwd)/../libsnappy/build -DO_LARGEFILE=0x0 -Dpread64=pread" CXXFLAGS="-fPIC -I$(pwd)/../libsnappy -I$(pwd)/../libsnappy/build -DO_LARGEFILE=0x0 -Dpread64=pread -std=c++11" LDFLAGS="-L$(pwd)/../libsnappy/build" RAPTOR2_LIBS="-L$(pwd)/../libraptor2/src/.libs -lraptor2" RAPTOR2_CFLAGS="-I$(pwd)/../libraptor2/src" ./configure --disable-shared --enable-static
-    echo
-    read -p "Ready to compile LIBAFF4?"
-    make -j$JOBS
-  )
-fi
+  fi
+  echo
+  read -p "Ready to compile LIBAFF4?"
+  make -j$JOBS
+)
 
 ############################## LibEWF
 (
